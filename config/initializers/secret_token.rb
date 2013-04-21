b248c163cc0a7b1fdcbee19801ebb383a319886f
PharaohKJ <pharaohkj@d142-087.jaist.ac.jp>
2013-04-21 12:27:43 +0900

    とりあえずbundle exec rails serverで動くようになったので初commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dsg::Application.config.secret_token = '029ee5907809fba4ed93aeb2c959c129acf1f4851bf86b67e78407ee61d9101c1591195660bc7debfe08edb8226a1a70ff543f53fdd6b6dcc3dbb57483c96c98'
