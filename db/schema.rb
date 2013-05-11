76d1cdf7a30641f75e7fdf85678dc009e9fc8027
PharaohKJ <phkjinc@gmail.com>
2013-05-11 16:55:20 +0900

    とりあえず初期レコード投入動き始め。

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,59 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20130421090401) do
+
+  create_table "mtb_among_card_expantions", :force => true do |t|
+    t.integer  "mtb_card_id"
+    t.integer  "mtb_expantion_id"
+    t.string   "override_name"
+    t.text     "override_text"
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
+  end
+
+  create_table "mtb_among_card_types", :force => true do |t|
+    t.integer  "mtb_card_id"
+    t.integer  "mtb_card_type_id"
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
+  end
+
+  create_table "mtb_card_types", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "mtb_cards", :force => true do |t|
+    t.string   "name"
+    t.string   "jname"
+    t.text     "text"
+    t.integer  "cost"
+    t.integer  "need_pot"
+    t.integer  "coin"
+    t.integer  "victory"
+    t.integer  "card"
+    t.integer  "action"
+    t.integer  "buy"
+    t.integer  "virtual_coin"
+    t.integer  "vitory_token"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+  end
+
+  create_table "mtb_expantions", :force => true do |t|
+    t.string   "name"
+    t.date     "released_at"
+    t.integer  "mtb_publisher_id"
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
+  end
+
+  create_table "mtb_publishers", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +7,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require "csv"
+
+basedir = 'db/master_data/'
+
+# mtb_publisher
+CSV.foreach( basedir + 'mtb_publisher.csv', :headers => :first_row) do |row|
+  MtbPublisher.create(
+                      :name => row['name']
+                      )
+end
+
+# mtb_card_type
+CSV.foreach(
+            basedir + 'mtb_card_type.csv',
+            :headers => :first_row,
+            :encoding => 'UTF-8'
+) do |row|
+  MtbCardType.create(
+                     :name => row['分類'])
+
+end
